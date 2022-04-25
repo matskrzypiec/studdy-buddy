652312d1464d3905962d62b000c17fd4fcb33f19
Mateusz <mateuszskrzypiec11@gmail.com>
2022-04-25 20:30:17 +0200

    Added atomic design, styles and state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,27 @@
-import {useState} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import UsersList from "./components/organisms/UsersList/UsersList";
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./assets/styles/globalStyles";
+import { theme } from "./assets/styles/theme";
+
+
+const Wrapper = styled.div`
+    background: ${({theme: {colors}}) => colors.lightGrey};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+`;
+
 
 const App = () => {
-    const [count, setCount] = useState(0)
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>Hello Vite + React!</p>
-                <p>
-                    <button type="button" onClick={() => setCount((count) => count + 1)}>
-                        count is: {count}
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.tsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {' | '}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
-        </div>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <Wrapper>
+                <UsersList />
+            </Wrapper>
+        </ThemeProvider>
     )
 }
 
